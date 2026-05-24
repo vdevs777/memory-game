9e1f51f88c7f9966579b25f0b1bfd5eb7880760d
vdevs777 <joaovsousamnm@gmail.com>
2026-05-24 15:27:37 -0300

    :tada: initial commit!

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@": "./src",
+          },
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+      ],
+      "react-native-worklets/plugin",
+    ],
+  };
+};
